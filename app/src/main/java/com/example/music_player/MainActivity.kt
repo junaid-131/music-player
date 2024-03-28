4802de759c5a9672675c145667000f52fd6e7930
junaid-131 <jk2673114@gmail.com>
2024-03-28 18:26:03 +0500

    first commit

--- app/src/main/java/com/example/music_player/MainActivity.kt
+++ app/src/main/java/com/example/music_player/MainActivity.kt
@@ -1,30 +1,72 @@
 package com.example.music_player
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
+
+
 import androidx.core.app.ActivityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.music_player.databinding.ActivityMainBinding
+import java.io.File
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    private lateinit var toggle: ActionBarDrawerToggle
+   /* private lateinit var toggle: ActionBarDrawerToggle*/
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var musicAdapter: musicAdapter
 
+    companion object{
+       lateinit var  MusicListMA:ArrayList<music>
+    }
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestRunTimePermission()
         setTheme(R.style.Theme_Music_player)
         setContentView(binding.root)
 
-            /*  for actionBarDrawer   */
-    toggle = ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
-        binding.root.addDrawerListener(toggle)
+
+
+        /*  for actionBarDrawer   */
+        /*  toggle = ActionBarDrawerToggle(this, binding.root,R.string.open, R.string.close)
+        binding.drawerlayout.addDrawerListener(toggle)
         toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)       */
+
+        drawerLayout = binding.drawerlayout
+        binding.openDrawer.setOnClickListener {
+            drawerLayout.open()
+        }
+
+        binding.navview.setNavigationItemSelectedListener{
+            when(it.itemId)
+            {
+                R.id.navFeedback -> Toast.makeText(this, "feedback", Toast.LENGTH_SHORT).show()
+                R.id.navAbout -> Toast.makeText(this, "About", Toast.LENGTH_SHORT).show()
+                R.id.navSetting -> Toast.makeText(this , "setting", Toast.LENGTH_SHORT).show()
+                R.id.navExit -> exitProcess(1)
+            }
+            true
+        }
+
+       MusicListMA = GettingAllAudio()
+
+        binding.musicRV.setHasFixedSize(true)
+        binding.musicRV.setItemViewCacheSize(13)
+        binding.musicRV.layoutManager = LinearLayoutManager(this)
+        musicAdapter = musicAdapter(this, MusicListMA)
+        binding.musicRV.adapter = musicAdapter
+        binding.totalSongs.text = "Total Songs : "+ musicAdapter.itemCount
 
 
 
@@ -72,4 +114,50 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /*      getting all audio files from storage  */
+
+    private fun GettingAllAudio(): ArrayList<music> {
+        val templist = ArrayList<music>()
+
+        val selection = MediaStore.Audio.Media.IS_MUSIC + " !=0"
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.DATE_ADDED,
+            MediaStore.Audio.Media.DATA
+        )
+
+        val cursor = this.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            null,
+            MediaStore.Audio.Media.DATE_ADDED + " DESC",
+            null
+        )
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val titleC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
+                val idC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID))
+                val albumC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
+                val artistC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+                val pathC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
+                val durationC = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION))
+
+                val music = music(id = idC, title = titleC, album = albumC, artist = artistC, path = pathC, duration = durationC)
+                val file = File(music.path)
+                if (file.exists())
+                    templist.add(music)
+            }
+            cursor.close()
+        }
+        return templist
+    }
+
+
+    
 }
--- app/src/main/java/com/example/music_player/PlayerActivity.kt
+++ app/src/main/java/com/example/music_player/PlayerActivity.kt
@@ -2,6 +2,7 @@ package com.example.music_player
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.example.music_player.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
@@ -12,5 +13,6 @@ class PlayerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_Music_player)
         setContentView(binding.root)
+
     }
 }
